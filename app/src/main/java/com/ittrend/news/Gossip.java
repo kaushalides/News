f5f0a3812ac201f5691f87e5a7b36351363a0fbc
rkaushi413@gmail.com <Mageruchi1221>
2019-01-21 19:40:53 +0530

    initial project

--- app/src/main/java/com/ittrend/news/Gossip.java
+++ app/src/main/java/com/ittrend/news/Gossip.java
@@ -7,16 +7,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
 public class Gossip extends Fragment {
 
-
+    static final String[] news =
+            new String[] { "gossip_news1", "gossip_news2", "gossip_news3", "gossip_news4"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gossip, container, false);
+        View view = inflater.inflate(R.layout.fragment_gossip, container, false);
+
+        final ListView list = view.findViewById(R.id.listview);
+        NewsAdpter adapter = new NewsAdpter(getActivity(),news);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println(list.getItemAtPosition(position)+"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            }
+        });
+        return view;
     }
 
 }
