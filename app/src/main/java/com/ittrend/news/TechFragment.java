fab5b74f6c3b5dda844d980cd9f728aaaa902959
rkaushi413@gmail.com <Mageruchi1221>
2019-01-21 19:41:55 +0530

    initial project

--- app/src/main/java/com/ittrend/news/TechFragment.java
+++ app/src/main/java/com/ittrend/news/TechFragment.java
@@ -7,15 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 public class TechFragment extends Fragment {
-
+    static final String[] news =
+            new String[] { "tech_news1", "tech_news2", "tech_news3", "tech_news4"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tech, container, false);
+        View view = inflater.inflate(R.layout.fragment_tech, container, false);
+        ListView list = view.findViewById(R.id.listview);
+        NewsAdpter adapter = new NewsAdpter(getActivity(),news);
+        list.setAdapter(adapter);
+        return view;
     }
 
 }
